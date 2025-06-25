3a597fca0c3fa819d82f03356b6a9b7d9fdce4fd
suraj-singh-c247 <suraj.p@chapter247.com>
2025-06-25 19:16:29 +0530

    Done some changes.

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -8,13 +8,15 @@ function EmployeeForm(props) {
         <>
             <form className='mb-3' onSubmit={(e) => onFunc(e)}>
                 <div className="mb-3">
-                    <label className="form-label">Upload Employee Data: </label>
-                    <input type="file" name='employeeData' accept=".xlsx, .xls, .ods" className="form-control" onChange={(e) => onFileUpload(e, setEmployeeData)} />
+                    <label for="employeeDataId" className="form-label">Upload Employee Data: </label>
+                    <input id="employeeDataId" type="file" name='employeeData' accept=".xlsx, .xls, .ods" className="form-control" onChange={(e) => onFileUpload(e, setEmployeeData)} />
+                    <p class="mt-1 fs-6 fw-light text-start" id="employeeDataId">You can upload files in the following formats: .xlsx, .xls, or .ods.</p>
                     {error.employeeData && !showFlagged && <p className="text-danger fw-bold py-3 text-start">{error.employeeData}</p>}
                 </div>
                 <div className="mb-3">
-                    <label className="form-label">Upload Payment Data: </label>
-                    <input type="file" name='paymentData' accept=".xlsx, .xls, .ods" className="form-control" onChange={(e) => onFileUpload(e, setPaymentData)} />
+                    <label for="emloyeePaymentId" className="form-label">Upload Payment Data: </label>
+                    <input id="emloyeePaymentId" type="file" name='paymentData' accept=".xlsx, .xls, .ods" className="form-control" onChange={(e) => onFileUpload(e, setPaymentData)} />
+                    <p class="mt-1 fs-6 fw-light text-start" id="emloyeePaymentId">You can upload files in the following formats: .xlsx, .xls, or .ods.</p>
                     {error.paymentData && !showFlagged && <p className="text-danger fw-bold py-3 text-start">{error.paymentData}</p>}
                 </div>
                 <Button type={"submit"} className={"btn btn-primary"} btnText={"Submit"} />
